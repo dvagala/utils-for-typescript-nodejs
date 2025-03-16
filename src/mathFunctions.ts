08531fcdeb340d44afc2fabf0bfbf5e7b3418ac7
Dominik Vagala <d.vagala@gmail.com>
2025-03-16 15:37:02 +0100

    update

--- src/mathFunctions.ts
+++ src/mathFunctions.ts
@@ -2,7 +2,7 @@ export function my_add(a: number, b: number): number {
   return a + b;
 }
 
-export function my_add_3(a: number, b: number): number {
+export function my_add_4(a: number, b: number): number {
   return a + b;
 }
 
