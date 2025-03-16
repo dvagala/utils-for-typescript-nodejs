ea77d05e260899cc527d50e11c337ac93181b590
Dominik Vagala <d.vagala@gmail.com>
2025-03-16 16:15:03 +0100

    modify some code

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-export * from './functions.js';
-export * from './parallel_section.js';
-export * from './extensions.js';
+// export * from './functions.js';
+// export * from './parallel_section.js';
+// export * from './extensions.js';
+export * from './mm.js';
