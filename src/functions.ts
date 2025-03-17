a42bcd39be12056ef92673e3ec89ffa0a2a2d8aa
Dominik Vagala <d.vagala@gmail.com>
2025-03-17 10:19:51 +0100

    modify some code

--- src/functions.ts
+++ src/functions.ts
@@ -57,6 +57,26 @@ export class NamedMutexes {
   }
 }
 
+export class DevStopwatch {
+  startTime: number;
+
+  constructor() {
+    this.startTime = performance.now();
+  }
+
+  public printElapsed(message: string = '') {
+    print(`Time to compute ${message}, takes: ${this.elapsedMs()} ms`);
+  }
+
+  public elapsedMs(): number {
+    return Math.round(performance.now() - this.startTime);
+  }
+
+  public elapsedS(): number {
+    return this.elapsedMs() / 1000;
+  }
+}
+
 // ----------------------------
 
 type NonFunctionPropertyNames<T> = {
