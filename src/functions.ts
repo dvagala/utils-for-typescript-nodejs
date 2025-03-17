865e4f074d1f2a35d90edbdac93a1519551da82a
Dominik Vagala <d.vagala@gmail.com>
2025-03-17 10:18:20 +0100

    modify some code

--- src/functions.ts
+++ src/functions.ts
@@ -3,6 +3,7 @@ import * as fs from 'fs';
 import { DateTime } from 'luxon';
 import { distance } from 'fastest-levenshtein';
 import { longestCommonSubstring } from 'string-algorithms';
+import { Mutex } from 'async-mutex';
 
 export interface PrintOptions {
   ignoreTruncation?: boolean;
@@ -35,6 +36,24 @@ export class ErrorWithRetryInfo extends Error {
   constructor(msg: string, public retryInfo: { canRetryRequest: boolean }) {
     super(msg);
     Object.setPrototypeOf(this, ErrorWithRetryInfo.prototype);
+  }
+}
+
+export class NamedMutexes {
+  private listOfMutexes: { [key: string]: Mutex } = {};
+
+  async acquire(options: { key: string }): Promise<void> {
+    if (this.listOfMutexes[options.key] == undefined) {
+      this.listOfMutexes[options.key] = new Mutex();
+    }
+
+    await this.listOfMutexes[options.key].acquire();
+  }
+
+  release(options: { key: string }) {
+    if (this.listOfMutexes[options.key] != null) {
+      this.listOfMutexes[options.key].release();
+    }
   }
 }
 
