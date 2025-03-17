5188ca4277ebda0fc464b20bf6cf8e7df0be1d9e
Dominik Vagala <d.vagala@gmail.com>
2025-03-17 15:23:25 +0100

    modify some code

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 export * from './utils/misc_utils.js';
-export * from './models/parallel_section.js';
-export * from './extensions.js';
 export * from './utils/wait_utils.js';
-export * from './models/dev_stopwatch.js';
-export * from './models/named_mutexes.js';
 export * from './utils/files_utils.js';
 export * from './utils/error_utils.js';
 export * from './utils/string_utils.js';
@@ -11,4 +7,11 @@ export * from './utils/array_utils.js';
 export * from './utils/math_utils.js';
 export * from './utils/json_utils.js';
 export * from './utils/date_utils.js';
+
+export * from './models/parallel_section.js';
+export * from './models/dev_stopwatch.js';
+export * from './models/named_mutexes.js';
 export * from './models/non_function_properties.js';
+export * from './models/periodic_runner.js';
+
+export * from './extensions.js';
